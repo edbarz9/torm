c9341f1bb613022b2eb728edb0228b1f493a748f
edbarz9 <charles@tinson.cc>
2019-03-14 21:36:54 +0100

    so far it can get the first page generated by the query

---
+++ tdown.py
@@ -0,0 +1,44 @@
+import os
+import sys
+from urllib.request import urlopen as uOpn
+from urllib.request import urlretrieve as uRtv
+from bs4 import BeautifulSoup as bsoup
+from urllib.request import Request
+
+piratebay = "https://pirateproxy.bet/search/"
+
+torrentClient = "transmission-gtk"
+
+def soupMaker(url):
+    req = Request(url,headers={'User-Agent': 'Mozilla/5.0'})
+    uClient = uOpn(req)
+    html_page = uClient.read()
+    uClient.close()
+    soup = bsoup(html_page,"html.parser")
+    return soup
+
+def parseQuery():
+    query = ""
+    i = -1
+    for i in range(len(sys.argv) - 2):
+        query = query + sys.argv[i+1] + "%20"
+
+    query = query + sys.argv[i+2]
+    return query
+
+query = parseQuery()
+
+#print(query)
+qurl = piratebay + query
+#print(qurl)
+#print(type(qurl))
+
+soup = soupMaker(qurl)
+
+#torrentlist = soup.find('tbody')
+
+print(soup)
+
+#os.system(torrentClient + " " + magnet)
+
+
